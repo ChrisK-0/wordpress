880ea145e8e4b7c6da2fdbf5c452bf54d6a2b528
chris <chris.kottisse@khk.ee>
2020-12-17 12:10:24 +0200

    modified plugin `Very First Plugin (welcome message)` version 1.0 (after deactivation of `Very First Plugin` version 1.0)
    	Conflict: wp-content/plugins/very-first-plugin.php [added]

---
+++ wp-content/plugins/very-first-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Very First Plugin (welcome message)
+ * Plugin URI: http://chriskottisse.ikt.khk.ee/
+ * Version: 1.0
+ * Author: Chris Kottisse
+ * Author URI: http://chriskottisse.ikt.khk.ee/
+**/
+
+[html]
+< ?php global $user_ID; if( $user_ID ) : ?>
+< ?php if( current_user_can(‘level_10’) ) : ?>
+
+    Tere tulemast!< ?php global $current_user; get_currentuserinfo(); echo ($current_user->user_login); ?>
+< ?php else : ?>
+< ?php endif; ?>
+< ?php endif; ?>
+[/html]
