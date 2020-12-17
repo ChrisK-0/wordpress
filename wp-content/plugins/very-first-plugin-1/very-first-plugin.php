5c871db44935c1a03d0709831e971f048482b3e7
chris <chris.kottisse@khk.ee>
2020-12-17 12:42:11 +0200

    modified plugin `Very First Plugin` version 1.0 (after activation of `Valentine's Day` version 1.0)

--- wp-content/plugins/very-first-plugin-1/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin-1/very-first-plugin.php
@@ -7,3 +7,17 @@
  * Author URI: http://chriskottisse.ikt.khk.ee/
 **/
 
+function alert_message()
+{
+    ?>
+    <script type="text/javascript">
+        function codeAddress() {
+            alert('Tere tulemast');
+        }
+        window.onload = codeAddress;
+    </script>
+
+    <?php
+}
+
+add_action('wp_head', 'alert_message');
