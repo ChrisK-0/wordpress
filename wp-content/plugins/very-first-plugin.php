801c6c2c46be12356d064dd89e01a986e6b6dbd7
chris <chris.kottisse@khk.ee>
2020-12-17 12:10:02 +0200

    modified plugin `Very First Plugin` version 1.0 (after deactivation of `Very First Plugin` version 1.0)
    	Conflict: wp-content/plugins/very-first-plugin.php [local version]

--- wp-content/plugins/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Very First Plugin (welcome message)
+ * Plugin Name: Very First Plugin
  * Plugin URI: http://chriskottisse.ikt.khk.ee/
  * Version: 1.0
  * Author: Chris Kottisse
