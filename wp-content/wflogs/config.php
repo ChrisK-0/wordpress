f4cc08e0377548abb7d62ce067fba2830114bc9c
chris <chris.kottisse@khk.ee>
2021-01-11 08:48:56 +0200

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1608288706;s:7:"authKey";s:64:"`7~Uctd8V0k~4^Gs>ItFij46,GcS#eVd5Q7ThVV0V{OPU$L-q;0G9((Ywnc6C{u7";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3548;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"`7~Uctd8V0k~4^Gs>ItFij46,GcS#eVd5Q7ThVV0V{OPU$L-q;0G9((Ywnc6C{u7";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3548;}
